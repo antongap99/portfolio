701c1ccf1ee5cebb738819381790b27cc23422ee
Anton.gap <anton.gap99@gmail.com>
2022-12-19 15:56:11 +0300

    feat: some changes

--- script/index.js
+++ script/index.js
@@ -1,11 +1,14 @@
 import swiperController from './swiper.js';
 import modalController from './modal.js';
+import formController from './form.js'
 
 const init = () => {
     swiperController();
     modalController({modal: '.modal1',
        btnOpen: '.present__order-btn',
        btnClose: '.modal__close',});
+
+    formController();
 }
 
 init()
