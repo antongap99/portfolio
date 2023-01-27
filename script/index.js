8009efaaddb6b593e827b282ff73d1a5a1e2c3df
Anton <anton.gap99@gmail.com>
2023-01-27 12:44:51 +0300

    fix: burger click

--- script/index.js
+++ script/index.js
@@ -1,11 +1,16 @@
 import swiperController from './swiper.js';
 import modalController from './modal.js';
+import {burgerController} from './burger.js'
 
 const init = () => {
     swiperController();
-    modalController({modal: '.modal1',
-       btnOpen: '.present__order-btn',
-       btnClose: '.modal__close',});
+    modalController({
+        modal: '.modal1',
+        btnOpen: '.present__order-btn',
+        btnClose: '.modal__close',
+    });
+
+    burgerController();
 }
 
 init()
